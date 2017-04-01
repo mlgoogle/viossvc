33f02809a604e87c308ab2ca1266254c97bd74c1
Done.L <823890952@qq.com>
2017-04-01 16:05:52 +0800

    refact: update test host and port
    update test host and port

--- viossvc/Marco/AppConst.swift
+++ viossvc/Marco/AppConst.swift
@@ -84,9 +84,9 @@ class AppConst {
         #if true //是否测试环境
 //        static let TcpServerIP:String = "192.168.199.131"
 //        static let TcpServerPort:UInt16 = 10001
-        static let TcpServerIP:String = "61.147.114.78"
-        static let TcpServerPort:UInt16 = 10011
-        static let TttpHostUrl:String = "http://61.147.114.78";
+        static let TcpServerIP:String = "139.224.34.22"
+        static let TcpServerPort:UInt16 = 10001
+        static let TttpHostUrl:String = "http://139.224.34.22";
         #else
         static let TcpServerIP:String = "103.40.192.101";
         static let TcpServerPort:UInt16 = 10002
